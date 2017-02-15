a35fedaa5698262a187952e774eab9b028133799
Manuel Araoz <manuelaraoz@gmail.com>
2017-02-15 13:32:20 -0300

    refactor names

--- test/TestProxyLibrary.sol
+++ test/TestProxyLibrary.sol
@@ -21,7 +21,7 @@ contract LibraryProxy {
 }
 
 // Contract you're testing
-contract Thrower {
+contract SampleLib {
   function doThrow() {
     throw;
   }
@@ -34,17 +34,13 @@ contract Thrower {
 
 contract TestProxyLibrary {
   function testThrow() {
-    Thrower thrower = new Thrower();
-    LibraryProxy throwProxy = new LibraryProxy(address(thrower)); //set Thrower as the contract to forward requests to. The target.
+    SampleLib lib = new SampleLib();
+    LibraryProxy proxy = new LibraryProxy(lib); 
 
-    //prime the proxy.
-    Thrower(address(throwProxy)).doThrow();
-    //execute the call that is supposed to throw.
-    //r will be false if it threw. r will be true if it didn't.
-    //make sure you send enough gas for your contract method.
-    bool r = throwProxy.execute.gas(200000)();
+    SampleLib(proxy).doThrow();
+    bool rf = proxy.execute.gas(200000)();
+    Assert.isFalse(rf, "Should be false, as it should throw");
 
-    Assert.isFalse(r, "Should be false, as it should throw");
   }
 }
 
