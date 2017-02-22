5c9334e075ab479734a1b2d77c3f0a17247fc504
Jorge Izquierdo <jorge@izqui.me>
2017-02-22 13:58:26 +0100

    Make dispatcher read msg.sig instead of using assembly

--- contracts/Dispatcher.sol
+++ contracts/Dispatcher.sol
@@ -4,11 +4,8 @@ import "./DispatcherStorage.sol";
 
 contract Dispatcher {
   function() {
-    bytes4 sig;
-    assembly { sig := calldataload(0) }
-
     DispatcherStorage dispatcherStorage = DispatcherStorage(0x1111222233334444555566667777888899990000);
-    uint32 len = dispatcherStorage.sizes(sig);
+    uint32 len = dispatcherStorage.sizes(msg.sig);
     address target = dispatcherStorage.lib();
 
     assembly {
